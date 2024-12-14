4d7dc86c9bde70716502fbce1d35b898564893cc
esrabrown <essrcskn@hotmail.com>
2024-12-13 22:21:03 -0600

    2.week solutions

---
+++ 2024-12-09/1.validAnagram.java
@@ -0,0 +1,42 @@
+/*
+Given two strings s and t, return true if t is an
+anagram of s, and false otherwise.
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+
+Output: true
+
+Example 2:
+
+Input: s = "rat", t = "car"
+
+Output: false
+ */
+
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        Map<Character, Integer> count = new HashMap<>();
+
+        // Count the frequency of characters in string s
+        for (char x : s.toCharArray()) {
+            count.put(x, count.getOrDefault(x, 0) + 1);
+        }
+
+        // Decrement the frequency of characters in string t
+        for (char x : t.toCharArray()) {
+            count.put(x, count.getOrDefault(x, 0) - 1);
+        }
+
+        // Check if any character has non-zero frequency
+        for (int val : count.values()) {
+            if (val != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ 2024-12-09/5.longestConsecutiveSequence.java
@@ -0,0 +1,43 @@
+/*
+Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+You must write an algorithm that runs in O(n) time.
+
+Example 1:
+
+Input: nums = [100,4,200,1,3,2]
+Output: 4
+Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+Example 2:
+
+Input: nums = [0,3,7,2,5,8,4,6,0,1]
+Output: 9
+ */
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        int n = nums.length;
+
+        if (n == 0) {
+            return 0;
+        }
+
+        Arrays.sort(nums);
+
+        int cnt = 1;
+        int maxi = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] != nums[i - 1]) {
+                if (nums[i] == nums[i - 1] + 1) {
+                    cnt++;
+                } else {
+                    maxi = Math.max(maxi, cnt);
+                    cnt = 1;
+                }
+            }
+        }
+
+        return Math.max(maxi, cnt);
+    }
+}
